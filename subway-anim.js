f158f68601a536d6a4524879370cb6f39fb9d407
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2023-06-04 00:18:24 +0200

    feat: Add moving train

---
+++ subway-anim.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener('keydown', function (event) {
+        if (event.code == 'ArrowRight') {
+            var subwayCar = document.getElementById('subway-car');
+            subwayCar.classList.add('moving-out');
+
+            setTimeout(function () {
+                subwayCar.classList.remove('moving-out');
+                subwayCar.classList.add('moving-in');
+                setTimeout(function () {
+                    subwayCar.classList.remove('moving-in');
+                }, 2000); // Remove the moving-in class after the animation is done
+            }, 2000); // Wait for the moving-out animation to finish before starting the moving-in animation
+        }
+    });
+});
+
